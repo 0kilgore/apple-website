db2aef8fa730e2d3f897e07e2386208c1affcfd1
Project <121197590+Barney010@users.noreply.github.com>
2023-02-11 10:19:13 -0500

    Add files via upload

---
+++ apple.js
@@ -0,0 +1,29 @@
+function displayContent(content) {
+    switch (content) {
+      case 'home':
+        window.location.href = 'apple.html';
+        break;
+      case 'Job':
+        window.location.href = 'job.html';
+        break;
+      case 'about':
+        window.location.href = 'about.html';
+        break;
+      case 'support':
+        window.location.href = 'support.html';
+        break;
+      case 'accessories':
+        window.location.href = 'accessories.html';
+        break;
+      case 'fix it':
+        window.location.href = 'fix it.html';
+        break;
+      case 'mac':
+        window.location.href = 'mac.html';
+        break;
+      default:
+        break;
+    }
+  }
+
+
